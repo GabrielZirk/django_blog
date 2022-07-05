e0699d37950b193e8c0e644fc2cb41727da03d6b
Gabriel Zirkovits <gabriel.zirkovits@edu.campus02.at>
2022-07-05 12:34:52 +0200

    Added db functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from blog.models import Post, Author, Tag
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "author", "date")
+    ordering = ('date',)
+    
+    class Meta:
+        verbose_name_plural = "Post"
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "mail")
+    ordering=('last_name', 'first_name')
+    
+    class Meta:
+        verbose_name_plural = "Author"
+        
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("caption",)
+    
+    class Meta:
+        verbose_name_plural = "Tag"
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,78 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from datetime import date
+from blog.models import Post
 
-# Create your views here.
-
-posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Gabi",
-        "date": date(2021, 7, 21),
-        "title": "Doggo",
-        "excerpt": "I love dogs",
-        "content": """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo 
-        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et 
-        ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."""
-    },
-    {
-        "slug": "catto",
-        "image": "woods.jpg",
-        "author": "Gabi",
-        "date": date(2021, 6, 21),
-        "title": "Catto",
-        "excerpt": "I love cats",
-        "content": """AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"""
-    },
-    {
-        "slug": "animalo",
-        "image": "coding.jpg",
-        "author": "Gabi",
-        "date": date(2021, 5, 21),
-        "title": "Animalo",
-        "excerpt": "I love all animals",
-        "content": """BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-        BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-        """
-    },
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Gabi",
-        "date": date(2021, 4, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There is nothing like the views you get when hiking in the mountians",
-        "content": """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo 
-        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et 
-        ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."""
-    }
-]
+# Create your views here
 
 def get_date(post):
     #print(post['date'])
     return post['date']
 
 def index(request):
-    sorted_posts = sorted(posts, key=lambda x: x['date'], reverse=True)
+    sorted_posts = Post.objects.all().order_by('-date')[:3] # This whole expression is converted into a SQL-command => better performance. Thanks Django :)
     latest_posts = sorted_posts[0:3]
     print(latest_posts)
     return render(request, 'blog/index.html', {'posts': latest_posts})
 
 
 def all_posts(request):
-    return render(request, 'blog/all_posts.html', {'all_posts': posts})
+    all_posts = Post.objects.all().order_by('date')
+    return render(request, 'blog/all_posts.html', {'all_posts': all_posts})
 
 
 def single_post(request, slug):
-    identified_post = next(post for post in posts if post['slug'] == slug)
-    return render(request, 'blog/post-detail.html', {"post": identified_post})
+    identified_post = get_object_or_404(Post, slug=slug)
+    print(identified_post)
+    return render(request, 'blog/post-detail.html', {"post": identified_post,
+                                                     "post_tags": identified_post.tags.all()})
 
 
